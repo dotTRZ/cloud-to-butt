fb750058e3a72333e8f829213c70bad9b0a003cc
dotTRZ <torin@rozzelle.net>
2015-12-07 23:17:20 -0500

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bTrump\b/g, "Literally Hitler");
+	v = v.replace(/\bDonald Trump\b/g, "Literally Hitler");
+	v = v.replace(/\btrump\b/g, "literally hitler");
+	v = v.replace(/\bdonald trump\b/g, "literally hitler");
 	
 	textNode.nodeValue = v;
 }
